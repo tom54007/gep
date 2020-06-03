083a004a35cd93b6110b756095a6449eb6b8df28
墨水 <752138607@qq.com>
2020-06-03 17:39:36 +0800

    test

--- basework/admin.py
+++ basework/admin.py
@@ -29,7 +29,7 @@ class ConstantEgAdmin(admin.ModelAdmin):
 
 class Annual_dataAdmin(admin.ModelAdmin):
     list_display = ('province', 'area', 'year')
-    fieldsets = (('年度数据', {
+    fieldsets = (('地区年度数据', {
             'fields': (
                 (
                     'province', 'area', 'year',
@@ -48,5 +48,87 @@ class Annual_dataAdmin(admin.ModelAdmin):
     )
 
 
+class Prov_Annual_dataAdmin(admin.ModelAdmin):
+    list_display = ('province', 'year')
+    fieldsets = (('省级年度数据', {
+            'fields': (
+                (
+                    'province', 'year',
+                ),
+                (
+                    'Consume_Raw_coal', 'Consume_Clean_coal','Consume_Coke','Consume_Briquette','Consume_Other_coking_products','Consume_Crude','Consume_Fuel_oil','Consume_Gasoline','Consume_Diesel','Consume_Kerosene','Consume_Liquefied_petroleum_gas','Consume_Refinery_dry_gas','Consume_Naphtha','Consume_Asphalt','Consume_Lubricating_oil','Consume_Petroleum_coke','Consume_Natural_gas',
+                ),
+                (
+                    'Consume_Cement', 'Consume_Steel',
+                ),
+                (
+                    'Consume_Farmland','Consume_Woodland','Consume_Pastureland','Consume_Fishing_ground','Consume_Construction_land',
+                ),
+            )
+        }),
+    )
+
+class Calculated_valueAdmin(admin.ModelAdmin):
+    list_display = ('province', 'area', 'year')
+    fieldsets = (('地区年度数据计算', {
+            'fields': (
+                (
+                    'province', 'area', 'year',
+                ),
+                (
+                    'Cal_Raw_coal', 'Cal_Clean_coal','Cal_Coke','Cal_Briquette','Cal_Other_coking_products','Cal_Crude','Cal_Fuel_oil','Cal_Gasoline','Cal_Diesel','Cal_Kerosene','Cal_Liquefied_petroleum_gas','Cal_Refinery_dry_gas','Cal_Naphtha','Cal_Asphalt','Cal_Lubricating_oil','Cal_Petroleum_coke','Cal_Natural_gas',
+                ),
+                (
+                    'GHG_Emission_a', 'CO2_Emission_a', 
+                ),
+                (
+                    'Cal_Cement', 'Cal_Steel',
+                ),
+                (
+                    'GHG_Emission_b', 'CO2_Emission_b', 
+                ),
+                (
+                    'Total_CO2_Emission',
+                ),
+                (
+                    'Cal_EF',
+                ),
+            )
+        }),
+    )
+
+class Prov_Calculated_valueAdmin(admin.ModelAdmin):
+    list_display = ('province', 'year')
+    fieldsets = (('省级年度数据计算', {
+            'fields': (
+                (
+                    'province', 'year',
+                ),
+                (
+                    'Cal_Raw_coal', 'Cal_Clean_coal','Cal_Coke','Cal_Briquette','Cal_Other_coking_products','Cal_Crude','Cal_Fuel_oil','Cal_Gasoline','Cal_Diesel','Cal_Kerosene','Cal_Liquefied_petroleum_gas','Cal_Refinery_dry_gas','Cal_Naphtha','Cal_Asphalt','Cal_Lubricating_oil','Cal_Petroleum_coke','Cal_Natural_gas',
+                ),
+                (
+                    'GHG_Emission_a', 'CO2_Emission_a', 
+                ),
+                (
+                    'Cal_Cement', 'Cal_Steel',
+                ),
+                (
+                    'GHG_Emission_b', 'CO2_Emission_b', 
+                ),
+                (
+                    'Total_CO2_Emission',
+                ),
+                (
+                    'Cal_EF',
+                ),
+            )
+        }),
+    )
+
+
+
 admin.site.register(ConstantEg, ConstantEgAdmin)
 admin.site.register(Annual_data, Annual_dataAdmin)
+admin.site.register(Prov_Annual_data, Prov_Annual_dataAdmin)
+admin.site.register(Calculated_value, Calculated_valueAdmin)
