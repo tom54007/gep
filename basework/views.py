61241f2dbd48d8c9f2993996a8bb846d8b011f0f
墨水 <752138607@qq.com>
2020-06-06 18:25:25 +0800

    test

--- basework/views.py
+++ basework/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from . import models
 
 
 # Create your views here.
 
+def importExcel(request):
+    context = {
+        'test': "数据导入"
+    }
+    return render(request, 'admin/basework/change_list_object_tools.html', context)
 
